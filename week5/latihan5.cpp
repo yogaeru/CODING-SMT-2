48b37d29f636a1a1654f92bf61b012bab068ee5a
yogaeru <yogaadhip3@gmail.com>
2025-03-13 16:07:37 +0700

    WEEK5

--- week5/latihan5.cpp
+++ week5/latihan5.cpp
@@ -26,9 +26,9 @@ public:
         tail = nullptr;
     }
 
-    // void push(const Mahasiswa &mahasiswa){
+    // void push(char code, std::string nama, long long int nim){
     //     Node *mhs = new Node();
-    //     mhs->value = mahasiswa;
+    //     mhs->value = {code, nama, nim};
 
     //     if(!head){
     //         head = mhs;
@@ -41,38 +41,94 @@ public:
     //     }
     // }
 
-    void push(char code, std::string nama, long long int nim){
+
+    void push(char code, std::string nama, long long int nim)
+    {
         Node *mhs = new Node();
         mhs->value = new Mahasiswa({code, nama, nim});
 
-        if(!head){
+        if (!head)
+        {
             head = mhs;
             tail = mhs;
         }
-        else{
+        else
+        {
             mhs->prev = tail;
             tail->next = mhs;
             tail = mhs;
         }
     }
 
-    void print(){
+    void print()
+    {
         Node *cur = tail;
-        while(cur){
+        while (cur)
+        {
+            std::cout << "+------------------------+\n";
             std::cout << "Mahasiswa " << cur->value->code << '\n';
             std::cout << "Nama : " << cur->value->nama << '\n';
-            std::cout << "NIM : " << cur->value->nim << '\n' << '\n';
+            std::cout << "NIM : " << cur->value->nim << "\n";
+            std::cout << "+------------------------+\n\n";
             cur = cur->prev;
         }
     }
 
-
-    ~linkedList(){
+    void printNim()
+    {
         Node *cur = head;
-        while (cur){
+        while (cur)
+        {
+            std::cout << cur->value->nim << '\n';
+            cur = cur->next;
+        }
+    }
+
+    void printNama()
+    {
+        Node *cur = tail;
+        while (cur)
+        {
+            std::cout << cur->value->nama << '\n';
+            cur = cur->prev;
+        }
+    }
+
+    void pop()
+    {
+        if (!head)
+            return;
+
+        if (head == tail)
+        {
+            delete head;
+            head = nullptr;
+            tail = nullptr;
+            std::cout << "Stack telah kosong";
+            return;
+        }
+
+        tail = tail->prev;
+        delete tail->next;
+        tail->next = nullptr;
+
+        Node *cur = tail;
+        while (cur)
+        {
+            std::cout << cur->value->nim << '\n';
+            cur = cur->prev;
+        }
+        std::cout << '\n';
+    }
+
+    ~linkedList()
+    {
+        Node *cur = head;
+        while (cur)
+        {
             Node *next = cur->next;
-            delete cur->value; // Hapus Mahasiswa yang dialokasikan dengan new
-            delete cur;        // Hapus Node
+            delete cur->value; 
+            delete cur; 
             cur = next;
         }
     }
@@ -81,9 +137,19 @@ public:
 int main()
 {
     linkedList a;
-    a.push('A', "yoga", 12123);
-    a.push('B', "Balmon", 1222);
+    a.push('A', "Yoga Adhi Prasetiyo", 24100);
+    a.push('B', "Nia", 24101);
+    a.push('C', "Nemo", 24102);
+    a.push('D', "Balmon", 24103);
     a.print();
-    
+    a.printNama();
+    std::cout << '\n';
+    a.printNim();
+    std::cout << '\n';
+    a.pop();
+    a.pop();
+    a.pop();
+    a.pop();
+
     return 0;
 }
