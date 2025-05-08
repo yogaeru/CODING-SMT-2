61472da4343ac3cdd97d131bd80ed53cfc4941d3
yogaeru <syamkupukz@gmail.com>
2025-05-08 21:20:26 +0700

    update sorting

---
+++ sorting/lib.cpp
@@ -0,0 +1,327 @@
+#include <iostream>
+#include <string>
+
+struct Mahasiswa {
+    std::string name;
+    long long int nim;
+    std::string gender;
+    std::string nilaiHuruf;
+    int nilaiAngka;
+};
+
+struct Node {
+    Mahasiswa *value = nullptr;
+    Node *next = nullptr;
+    Node *prev = nullptr;
+};
+
+struct linked_list {
+private:
+    Node *head, *tail;
+
+public:
+    linked_list() {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    ~linked_list() {
+        Node *cur = head;
+        while (cur) {
+            Node *del = cur;
+            cur = cur->next;
+            delete del;
+        }
+    }
+
+    [[nodiscard]] int length() const {
+        const Node *cur = head;
+        int leng = 0;
+        while (cur) {
+            cur = cur->next;
+            leng++;
+        }
+        return leng;
+    }
+
+    [[nodiscard]] float mean() const {
+        const Node *cur = head;
+        int total = 0;
+        int leng = 0;
+        while (cur) {
+            total += cur->value->nilaiAngka;
+            leng++;
+            cur = cur->next;
+        }
+        const float mean = static_cast<float>(total) / static_cast<float>(leng);
+        return mean;
+    }
+
+    void addMahasiswa(const std::string &nama,
+                      const long long int nim,
+                      const std::string &gender,
+                      const std::string &huruf,
+                      const int angka) {
+        Node *mhs = new Node();
+        mhs->value = new Mahasiswa{nama, nim, gender, huruf, angka};
+        if (!head) {
+            head = mhs;
+            tail = mhs;
+        } else {
+            mhs->prev = tail;
+            tail->next = mhs;
+            tail = mhs;
+        }
+    }
+
+    void cari(const long long int nim) const {
+        const Node *cur = head;
+        bool found = false;
+        while (cur) {
+            if (cur->value->nim == nim) {
+                found = true;
+                break;
+            }
+            cur = cur->next;
+        }
+        if (found) {
+            std::cout << "data ditemukan\n";
+            std::cout << "Nama: " << cur->value->name << "\n";
+            std::cout << "NIM: " << cur->value->nim << '\n';
+            std::cout << "Gender: " << cur->value->gender << '\n';
+            std::cout << "Nilai Angka " << cur->value->nilaiAngka << '\n';
+            std::cout << "Nilai huruf " << cur->value->nilaiHuruf << '\n';
+        } else {
+            std::cout << "data tidak ada\n";
+        }
+    }
+
+    [[nodiscard]] int nilaiTinggi() const {
+        const Node *cur = head;
+        int nilai = 0;
+        while (cur) {
+            if (cur == head) {
+                nilai += cur->value->nilaiAngka;
+                cur = cur->next;
+            } else {
+                if (cur->value->nilaiAngka > nilai) {
+                    nilai = cur->value->nilaiAngka;
+                }
+                cur = cur->next;
+            }
+        }
+        return nilai;
+    }
+
+    [[nodiscard]] int nilaiRendah() const {
+        const Node *cur = head;
+        int nilai = 0;
+        while (cur) {
+            if (cur == head) {
+                nilai = cur->value->nilaiAngka;
+                cur = cur->next;
+            } else {
+                if (cur->value->nilaiAngka < nilai) {
+                    nilai = cur->value->nilaiAngka;
+                }
+                cur = cur->next;
+            }
+        }
+        return nilai;
+    }
+
+    void print() const {
+        const Node *cur = head;
+        while (cur) {
+            std::cout << "Nama: " << cur->value->name << "\n";
+            std::cout << "NIM: " << cur->value->nim << '\n';
+            std::cout << "Gender: " << cur->value->gender << '\n';
+            std::cout << "Nilai Angka " << cur->value->nilaiAngka << '\n';
+            std::cout << "Nilai huruf " << cur->value->nilaiHuruf << '\n';
+            cur = cur->next;
+        }
+    }
+
+    void swapNode(Node *left, Node *right) {
+        if (left == right) return;
+        if (left == head) { head = right; }
+        if (right == tail) { tail = left; }
+
+        if (left->next == right) {
+            if (right->next) {
+                left->next = right->next;
+                right->next->prev = left;
+            }
+            if (left->prev) {
+                left->prev->next = right;
+                right->prev = left->prev;
+            }
+            right->next = left;
+            left->prev = right;
+        } else {
+            /***
+                Variabel untuk menyimpan Node setelah left
+                dan Node sebelum right
+             ***/
+            Node *tempLeftNext = left->next;
+            Node *tempRightPrev = right->prev;
+
+             // Untuk merubah pointer Node sebelumnya dan Node setelahnya
+            if (right->next) { right->next->prev = left; }
+            if (left->prev) { left->prev->next = right; }
+            if (tempLeftNext) { tempLeftNext->prev = right; }
+            if (tempRightPrev) { tempRightPrev->next = left; }
+
+            // menukar Node saat ini
+            right->prev = left->prev;
+            left->next = right->next;
+            right->next = tempLeftNext;
+            left->prev = tempRightPrev;
+        }
+
+        if (right == head) { right->prev = nullptr; }
+        if (left == tail) { left->next = nullptr; }
+    }
+
+    void bubble_sort(const std::string &data) {
+        int len = this->length(); //menentukan panjang Node
+        bool swapped = true;
+
+        while (swapped) {
+            swapped = false;
+            Node *left = head;
+            Node *right = left->next;
+
+            for (int step = 0; step < len - 1; step++) {
+                Node *tempLeft = left;
+                Node *tempRight = right;
+
+                //cek user ingin cari data bedasarkan apa
+                long long int leftValue = (data == "nim") ? tempLeft->value->nim : tempLeft->value->nilaiAngka;
+                long long int rightValue = (data == "nim") ? tempRight->value->nim : tempRight->value->nilaiAngka;
+
+                //jika value Node kiri lebih besar tukar posisinya
+                if (leftValue > rightValue) {
+                    this->swapNode(tempLeft, tempRight);
+                    swapped = true;
+                    right = tempLeft->next;
+                } else {
+                    left = left->next;
+                    right = right->next;
+                }
+                //kita ubah posisi Node kiri dan kanan
+                // left = (swapped) ? tempLeft : tempLeft->next;
+                // right = (swapped) ? tempLeft->next : right->next;
+
+                // if (!right) {
+                //     std::cout << "data sudah terurut\n";
+                //     return;
+                // }
+            }
+            len--;
+        }
+        std::cout << "data sudah terurut\n";
+    }
+
+    void selection_sort(const std::string &data) {
+        int len = this->length(); //menentukan panjang Node
+
+        Node *left = head;
+        Node *right = nullptr;
+        Node *minimum = nullptr;
+        Node *temp = nullptr;
+
+        for (int step = 0; step < len; step++) {
+            minimum = left;
+            right = minimum->next;
+
+            while (right) {
+                long long int rightValue = (data == "nim") ? right->value->nim : right->value->nilaiAngka;
+                long long int minValue = (data == "nim") ? minimum->value->nim : minimum->value->nilaiAngka;
+
+                if (minValue > rightValue) {
+                    minimum = right;
+                }
+                right = right->next;
+            }
+
+            temp = left->next;
+            if (minimum != left) {
+                this->swapNode(left, minimum);
+            }
+            left = temp;
+        }
+        std::cout << "data sudah terurut\n";
+    }
+
+    [[nodiscard("Return Node at index")]] Node *nodeAt(const int index, const int leng) const {
+        Node *cur = head;
+        if (index >= leng || index < 0) { return nullptr; }
+        for (int i = 0; i < index; i++) {
+            if (!cur) { return nullptr; }
+            cur = cur->next;
+        }
+        return cur;
+    }
+
+    void shell_sort(const std::string &data) {
+        int len = this->length();
+        int gap = len / 2;
+
+        // ciura sequence
+        // int gap_container[] = {701, 301, 132, 57, 23, 10, 4, 1};
+
+        // for (int gap: gap_container)
+        while (gap > 0) {
+            for (int i = gap; i < len; i++) {
+                int j = i;
+                // Node *temp = nodeAt(i, len);
+                while (j >= gap) {
+                    Node *left = this->nodeAt(j - gap, len);
+                    Node *right = this->nodeAt(j, len);
+
+                    long long int tempValue = (data == "nim") ? right->value->nim : right->value->nilaiAngka;
+                    long long int leftValue = (data == "nim") ? left->value->nim : left->value->nilaiAngka;
+
+                    if (leftValue > tempValue) {
+                        this->swapNode(left, right);
+                        j -= gap;
+                    } else { break; }
+                }
+            }
+            gap /= 2;
+            print();
+        }
+    }
+
+    [[nodiscard("return index I")]]
+    int partition(const int low, const int high, const std::string &data) {
+        int len = this->length();
+        int i = low - 1;
+
+        Node *pivot = nodeAt(high, len);
+        Node *nodeI = (i > 0) ? nodeAt(i, len) : nullptr;
+        Node *nodeJ = nodeAt(low, len);
+
+        long long int pivotValue = (data == "nim") ? pivot->value->nim : pivot->value->nilaiAngka;
+
+        for (int j = low; j < high; j++) {
+            Node *temp = nodeJ;
+            long long int tempValue = (data == "nim") ? temp->value->nim : temp->value->nilaiAngka;
+            if (tempValue <= pivotValue) {
+                i++;
+                if (!nodeI) {
+                    nodeI = head;
+                } else {
+                    nodeI = nodeI->next;
+                }
+                swapNode(nodeI, temp);
+            }
+            nodeJ = nodeJ->next;
+        }
+
+        nodeI = (nodeI) ? nodeI->next : head;
+        swapNode(nodeI, pivot);
+        return i + 1;
+    }
+};
---
+++ sorting/main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include "lib.cpp"
+
+void nambah(linked_list *a);
+
+void mainMenu(linked_list *a);
+
+void statistik(const linked_list *a);
+
+int main() {
+    auto data = new linked_list();
+    mainMenu(data);
+
+    delete data;
+    return 0;
+}
+
+
+void mainMenu(linked_list *a) {
+    std::string temp;
+    char choice;
+    while (true) {
+        std::cout << "PILIH MENU\n";
+        std::cout << "1. Tambah data\n";
+        std::cout << "2. Lihat data\n";
+        std::cout << "3. Statistik data\n";
+        std::cout << "4. Cari Mahasiswa\n";
+        std::cout << "5. Exit\n";
+        std::cout << "6. Shell Sort\n";
+
+        std::cin >> choice;
+        // std::cin.ignore();
+
+        switch (choice) {
+            case '1':
+                char confirm;
+                while (true) {
+                    nambah(a);
+                    std::cout << "Apkaah ingin menambahkan data lagi ? (y/n)\n";
+                    std::cin >> confirm;
+                    if (tolower(confirm) != 'y') { break; }
+                }
+                break;
+            case '2':
+                std::cout << "======= Data mahasiswa  ===== \n";
+                a->print();
+                std::cout << "Masukan apa saja untuk kembali...";
+                std::cin >> temp;
+                break;
+            case '3':
+                std::cout << "Statisti mahasisqa\n";
+                statistik(a);
+                std::cout << "Masukan apa saja untuk kembali...";
+                std::cin >> temp;
+                break;
+            case '4':
+                int nim;
+                std::cout << "MEncari mahasiswa\n";
+                std::cout << "Masukan Nim: ";
+                std::cin >> nim;
+                a->cari(nim);
+                break;
+            case '5':
+                std::cout << "Keluar......\n";
+                return;
+            case '6':
+                break;
+            case '7':
+                a->bubble_sort("nilai");
+                break;
+            case '8':
+                a->selection_sort("nilai");
+                break;
+            case '9':
+                a->shell_sort("nilai");
+                break;
+            default:
+                std::cout << "Pilihan tidak ada!!!!!!!\n";
+                break;
+        }
+    }
+}
+
+void nambah(linked_list *a) {
+    std::string nama, gender;
+    long long int nim;
+    std::string huruf;
+    int angka;
+
+    std::cout << "Nama: ";
+    std::cin >> nama;
+    std::cout << "NIM: ";
+    std::cin >> nim;
+    std::cin.ignore();
+    std::cout << "Gender: ";
+    std::cin >> gender;
+    std::cout << "Nilai: ";
+    std::cin >> angka;
+    std::cin.ignore();
+
+    if (angka >= 86) {
+        huruf = "A";
+    } else if (angka < 86 && angka >= 81) {
+        huruf = "AB";
+    } else if (angka < 81 && angka >= 70) {
+        huruf = "B";
+    } else if (angka < 70 && angka >= 65) {
+        huruf = "BC";
+    } else if (angka < 65 && angka >= 60) {
+        huruf = "C";
+    } else if (angka < 60 && angka >= 50) {
+        huruf = "D";
+    } else {
+        huruf = "E";
+    }
+
+    a->addMahasiswa(nama, nim, gender, huruf, angka);
+    std::cout << "data berhasil ditambah\n";
+}
+
+void statistik(const linked_list *a) {
+    const int nilai_rendah = a->nilaiRendah();
+    const int nilai_tinggi = a->nilaiTinggi();
+    const float mean = a->mean();
+    const int jumlahData = a->length();
+
+    std::cout << "Jumlah data mahasiswa = " << jumlahData << '\n';
+    std::cout << "Rata rata nilai mahasiswa = " << mean << '\n';
+    std::cout << "Nilai mahasiswa teredah = " << nilai_rendah << '\n';
+    std::cout << "Nilai mahasiswa tertinggi = " << nilai_tinggi << '\n';
+}
