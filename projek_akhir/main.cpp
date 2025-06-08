7953559e303b63460d71cb32debce5626283a9d6
yogaeru <syamkupukz@gmail.com>
2025-06-08 20:25:12 +0700

    Refactor

--- projek_akhir/main.cpp
+++ projek_akhir/main.cpp
@@ -4,26 +4,29 @@
 using std::string;
 
 void mainMenu(database *db);
+
 void nambahPasien(database *db);
+
 void bca(database *db, string type);
+
 void menuSorting(database *db);
+
 void abc(database *db, string type, string target);
+
 void menuSearch(database *db);
+
 void testingCode(database *db);
 
-int main()
-{
+int main() {
     database *dataPasien = new database();
     mainMenu(dataPasien);
     delete dataPasien;
     return 0;
 }
 
-void mainMenu(database *db)
-{
+void mainMenu(database *db) {
     string pilihan;
-    while (true)
-    {
+    while (true) {
         std::cout << "\033[2J\033[H";
         std::cout << "=================================\n";
         std::cout << "=========== MAIN MENU ===========\n\n";
@@ -35,57 +38,47 @@ void mainMenu(database *db)
         std::cout << "6. Exit\n\n";
         std::cout << "Masukkan pilihan: ";
         std::cin >> pilihan;
-        std::cin.ignore();
 
-        if (pilihan == "1")
-        {
+        if (pilihan == "1") {
             nambahPasien(db);
-        }
-        else if (pilihan == "2")
-        {
+        } else if (pilihan == "2") {
+            std::cin.ignore();
             db->print();
             std::cout << "Enter Untuk Kembali......";
             std::cin.get();
-        }
-        else if (pilihan == "3")
-        {
+        } else if (pilihan == "3") {
             menuSorting(db);
-        }
-        else if (pilihan == "4")
-        {
+        } else if (pilihan == "4") {
             menuSearch(db);
-        }
-        else if (pilihan == "5")
-        {
+        } else if (pilihan == "5") {
             std::cout << "belum ada FITUR\n";
-        }
-        else if (pilihan == "6")
-        {
+        } else if (pilihan == "6") {
             return;
-        }
-        else if (pilihan == "99")
-        {
+        } else if (pilihan == "99") {
             testingCode(db);
-        }
-        else
-        {
+        } else {
             std::cout << "YAHHH PILIHAN TIDAK ADA :( \n";
         }
     }
 }
 
-void nambahPasien(database *db)
-{
+
+// <======== MENAMBAHKAN DATA PASIEN ===========>
+// <======== MENAMBAHKAN DATA PASIEN ===========>
+// <======== MENAMBAHKAN DATA PASIEN ===========>
+// <======== MENAMBAHKAN DATA PASIEN ===========>
+// <======== MENAMBAHKAN DATA PASIEN ===========>
+void nambahPasien(database *db) {
     string nama, penyakit;
     char validasi;
     int nomor, umur;
 
-    while (true)
-    {
+    while (true) {
         std::cout << "\033[2J\033[H";
         std::cout << "=================================\n";
         std::cout << "==== MENAMBAHKAN DATA PASIEN ====\n\n";
-        std::cout << "Nama pasien   : ";
+        std::cout << "Nama pasien : ";
+        std::cin.ignore();
         std::getline(std::cin, nama);
         std::cout << "Penyakit    : ";
         std::getline(std::cin, penyakit);
@@ -104,12 +97,17 @@ void nambahPasien(database *db)
     }
 }
 
-void menuSorting(database *db)
-{
+
+//====================== MENU SORTING =============================
+//====================== MENU SORTING =============================
+//====================== MENU SORTING =============================
+//====================== MENU SORTING =============================
+//====================== MENU SORTING =============================
+//====================== MENU SORTING =============================
+void menuSorting(database *db) {
     string pilihan;
     string target;
-    while (true)
-    {
+    while (true) {
         std::cout << "\033[2J\033[H\n";
         db->print();
         std::cout << "=====================\n";
@@ -122,36 +120,60 @@ void menuSorting(database *db)
         std::cout << "Masukkan pilihan: ";
         std::cin >> pilihan;
 
-        if (pilihan == "1")
-        {
+        if (pilihan == "1") {
             bca(db, "nama");
-        }
-        else if (pilihan == "2")
-        {
+        } else if (pilihan == "2") {
             bca(db, "umur");
-        }
-        else if (pilihan == "3")
-        {
+        } else if (pilihan == "3") {
             bca(db, "nomor");
-        }
-        else if (pilihan == "4")
-        {
+        } else if (pilihan == "4") {
             return;
-        }
-        else
-        {
+        } else {
             std::cout << "YAHH PILIHAN TIDAK ADAAA :( \n";
         }
     }
 }
 
+void bca(database *db, string type) {
+    string pilihan;
+    while (true) {
+        std::cout << "\033[2J\033[H";
+        std::cout << "1. Bubble Sort\n";
+        std::cout << "2. Selection  Sort\n";
+        std::cout << "3. Insertion Sort\n";
+        std::cout << "4. Kembali\n";
+        std::cout << "Pilihan: ";
+        std::cin >> pilihan;
 
-void menuSearch(database *db)
-{
+        if (pilihan == "1") {
+            db->bubble_sort(type);
+            system("pause");
+            return;
+        } else if (pilihan == "2") {
+            db->urutkan_data(type);
+            system("pause");
+            return;
+        } else if (pilihan == "3") {
+            db->insertion_sort(type);
+            system("pause");
+            return;
+        } else if (pilihan == "4") {
+            return;
+        } else {
+            std::cout << "YAHH PILIHAN TIDAK ADA :(\n";
+        }
+    }
+}
+
+
+//====================== MENU SEARCHING ==================================
+//====================== MENU SEARCHING ==================================
+//====================== MENU SEARCHING ==================================
+//====================== MENU SEARCHING ==================================
+void menuSearch(database *db) {
     string pilihan, target;
 
-    while (true)
-    {
+    while (true) {
         std::cout << "\033[2J\033[H";
         std::cout << "1. Nama\n";
         std::cout << "2. Penyakit\n";
@@ -161,117 +183,38 @@ void menuSearch(database *db)
         std::cout << "Pilihan: \n";
         std::cin >> pilihan;
 
-        if (pilihan == "1")
-        {
+        if (pilihan == "1") {
             std::cout << "Masukkan nama: ";
             std::cin >> target;
             abc(db, "nama", target);
             system("pause");
-        }
-        else if (pilihan == "2")
-        {
+        } else if (pilihan == "2") {
             std::cout << "Masukkan penyakit: ";
             std::cin >> target;
             abc(db, "penyakit", target);
             system("pause");
-        }
-        else if (pilihan == "3")
-        {
+        } else if (pilihan == "3") {
             std::cout << "Masukkan umur: ";
             std::cin >> target;
             abc(db, "umur", target);
             system("pause");
-        }
-        else if (pilihan == "4")
-        {
+        } else if (pilihan == "4") {
             std::cout << "Masukkan nomor: ";
             std::cin >> target;
             abc(db, "nomor", target);
             system("pause");
-        }
-        else if (pilihan == "5")
-        {
+        } else if (pilihan == "5") {
             return;
-        }
-        else
-        {
+        } else {
             std::cout << "YAHH PILIHAN GK ADA :( \n";
             system("pause");
         }
     }
 }
 
-
-void testingCode(database *db)
-{
-    string nama[] = {"Aru", "Nana", "Ai"};
-    int umur[] = {20, 21, 22};
-    string penyakit[] = {"Cacar", "Batuk", "Flu"};
-
-    for (int i = 0; i < 3; i++)
-    {
-        db->addPasien(nama[i], umur[i], penyakit[i], i + 2);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-void bca(database *db, string type)
-{
+void abc(database *db, string type, string target) {
     string pilihan;
-    while (true)
-    {
-        std::cout << "\033[2J\033[H";
-        std::cout << "1. Bubble Sort\n";
-        std::cout << "2. Selection  Sort\n";
-        std::cout << "3. Insertion Sort\n";
-        std::cout << "4. Kembali\n";
-        std::cout << "Pilihan: ";
-        std::cin >> pilihan;
-
-        if (pilihan == "1")
-        {
-            db->bubble_sort(type);
-            system("pause");
-            return;
-        }
-        else if (pilihan == "2")
-        {
-            db->urutkan_data(type);
-            system("pause");
-            return;
-        }
-        else if (pilihan == "3")
-        {
-            db->insertion_sort(type);
-            system("pause");
-            return;
-        }
-        else if (pilihan == "4")
-        {
-            return;
-        }
-        else
-        {
-            std::cout << "YAHH PILIHAN TIDAK ADA :(\n";
-        }
-    }
-}
-
-
-void abc(database *db, string type, string target)
-{
-    string pilihan;
-    while (true)
-    {
+    while (true) {
         std::cout << "\033[2J\033[H";
         std::cout << "1. Linear Search\n";
         std::cout << "2. Bianry Search\n";
@@ -279,42 +222,46 @@ void abc(database *db, string type, string target)
         std::cout << "Pilih: ";
         std::cin >> pilihan;
 
-        if (pilihan == "1")
-        {
+        if (pilihan == "1") {
             // linear search
-            if (type == "nama" || type == "penyakit")
-            {
+            if (type == "nama" || type == "penyakit") {
                 db->cari_nama(type, target);
                 return;
-            }
-            else
-            {
+            } else {
                 db->cari_data(type, std::stoi(target));
                 return;
             }
-        }
-        else if (pilihan == "2")
-        {
+        } else if (pilihan == "2") {
             // binary search
-            if (type == "nama" || type == "penyakit")
-            {
+            if (type == "nama" || type == "penyakit") {
                 db->startBinarySearchStr(type, target);
                 return;
-            }
-            else
-            {
+            } else {
                 db->startBinarySearchNum(type, std::stoi(target));
                 return;
             }
-        }
-        else if (pilihan == "3")
-        {
+        } else if (pilihan == "3") {
             return;
-        }
-        else
-        {
+        } else {
             std::cout << "YAHH PILIHAN TIDAK ADA :(\n";
             std::cin.get();
         }
     }
 }
+
+
+// ====================================== TESTING CODE ===============================
+// ===================================== TESTING CODE ===============================
+// ====================================== TESTING CODE ===============================
+// ======================================== TESTING CODE ===============================
+// ======================================== TESTING CODE ===============================
+void testingCode(database *db) {
+    string nama[] = {"Aru", "Nana", "Ai"};
+    int umur[] = {20, 21, 22};
+    string penyakit[] = {"Cacar", "Batuk", "Flu"};
+
+    for (int i = 0; i < 3; i++) {
+        db->addPasien(nama[i], umur[i], penyakit[i], i + 2);
+    }
+}
+
